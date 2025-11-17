28de7f4c3f5b5f8d92a15e084038401397c7d204
ffgtfgh <152868614+ffgtfgh53@users.noreply.github.com>
2025-11-17 13:06:27 +0800

    better colour support updated menu

---
+++ src/init.rs
@@ -0,0 +1,20 @@
+#![allow(unused)]
+
+use pancurses::{self, COLOR_BLACK, COLOR_GREEN, COLOR_MAGENTA, COLOR_RED, COLOR_WHITE, curs_set, init_color, init_pair, noecho, start_color, use_default_colors};
+
+pub const BLACK: u32 = 0;
+pub const WHITE: u32 = 1;
+pub const GREEN: u32 = 2;
+pub const RED: u32 = 3;
+
+pub fn init() {
+    curs_set(0);
+    noecho();
+    start_color();
+    use_default_colors();
+
+    init_pair(BLACK as i16, COLOR_BLACK, -1);
+    init_pair(WHITE as i16, COLOR_WHITE, -1);
+    init_pair(GREEN as i16, COLOR_GREEN, -1);
+    init_pair(RED as i16, COLOR_RED, -1);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use pancurses::{curs_set, endwin, initscr, noecho};
+use pancurses::{endwin, initscr};
 
-use sliding_puzzle::{Cell, Level, run_level};
+use sliding_puzzle::{Cell, Level, Menuitems, menu, run_level};
 
 mod levels;
 
-extern crate pancurses;
+mod init;
 
 fn main() -> Result<(), String>{
 
@@ -14,12 +14,47 @@ fn main() -> Result<(), String>{
 
     let window = initscr();
     window.keypad(true);
-    curs_set(0);
-    noecho();
+    init::init();
 
-    run_level(&window, &mut level1, &"Level 1".to_string()).map_err(|e| {endwin(); e})?;
-    run_level(&window, &mut level2, &"Level 2".to_string()).map_err(|e| {endwin(); e})?;
-    run_level(&window, &mut level3, &"Level 3".to_string()).map_err(|e| {endwin(); e})?;
-    endwin();
-    Ok(())
+    macro_rules! endwin_return {
+        ($val:expr) => {{
+            endwin();
+            return $val
+        }};
+    }
+
+    macro_rules! handle {
+        ($result:expr) => {{
+            match $result {
+                Ok(Menuitems::Exit) => endwin_return!(Ok(())),
+                Ok(Menuitems::Next) => (),
+                Ok(Menuitems::Select) => (),
+                Err(e) => endwin_return!(Err(e))
+            }   
+        }};
+    }
+
+    match menu(&window)? {
+        Menuitems::Next => (),
+        Menuitems::Select => (), //change later
+        Menuitems::Exit => endwin_return!(Ok(()))
+    };
+
+    handle!(run_level(
+        &window, 
+        &mut level1, 
+        &"Level 1".to_string()));
+    handle!(run_level(
+        &window, 
+        &mut level2, 
+        &"Level 2".to_string()
+    ));
+    handle!(run_level(
+        &window, 
+        &mut level3, 
+        &"Level 3".to_string()
+    ));
+    
+    endwin_return!(Ok(()))
 }
+
